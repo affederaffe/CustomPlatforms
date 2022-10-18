5d42fdcdb4b6a5599d4c8317f9071237d133ff4d
Kevin Kylon <kylon99@gmail.com>
2022-10-17 22:37:24 -0700

    Fixed PrefabLightmapData for BeatSaber after change to Unity 2019

--- Plugin/CustomFloorPlugin/Behaviours/PrefabLightmapData.cs
+++ Plugin/CustomFloorPlugin/Behaviours/PrefabLightmapData.cs
@@ -1,56 +1,138 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.IO;
+using System.Linq;
 using CustomFloorPlugin.Interfaces;
+using Newtonsoft.Json;
 using UnityEngine;
-
 using Zenject;
 
 
 // ReSharper disable once CheckNamespace
 namespace CustomFloorPlugin
 {
+    public struct RendererInfo
+    {
+        public Renderer renderer;
+        public int lightmapIndex;
+        public Vector4 lightmapOffsetScale;
+    }
+    public struct LightInfo
+    {
+        public Light light;
+        public int lightmapBaketype;
+        public int mixedLightingMode;
+    }
+
+    /// <summary>
+    /// Enables the light mapping data saved in this PrefabLightmapData
+    /// Adapted from https://github.com/Ayfel/PrefabLightmapping for use in Beat Saber
+    /// </summary>
     public class PrefabLightmapData : MonoBehaviour, INotifyPlatformEnabled
     {
+        // This version of Unity can't reconstruct structs or classes no matter how much you mark 
+        // it up with SerializeField or Serializable. RendererInfo and LightInfo has to be expanded out.
+        // Is this because of Unity's use of a flat-JSON serializer?! WTF
+
         // ReSharper disable InconsistentNaming
-        public Renderer[]? m_Renderers;
-        public Vector4[]? m_LightmapOffsetScales;
+        public Renderer[]? renderInfo_renderer;
+        public int[]? renderInfo_lightmapIndex;
+        public Vector4[]? renderInfo_lightmapOffsetScale;
+
         public Texture2D[]? m_Lightmaps;
+        public Texture2D[]? m_LightmapsDir;
+        public Texture2D[]? m_ShadowMasks;
+
+        public Light[]? lightInfo_light;
+        public int[]? lightInfo_lightmapBaketype;
+        public int[]? lightInfo_mixedLightingMode;
         // ReSharper restore InconsistentNaming
 
         public void PlatformEnabled(DiContainer container)
         {
-            enabled = m_Renderers is not null && m_LightmapOffsetScales is not null && m_Lightmaps is not null && m_Renderers.Length > 0 &&
-                      m_Renderers.Length != m_LightmapOffsetScales.Length && m_Renderers.Length != m_Lightmaps.Length &&
-                      m_LightmapOffsetScales.Length != m_Lightmaps.Length &&
-                      m_Renderers[m_Renderers.Length - 1].lightmapIndex >= LightmapSettings.lightmaps.Length;
+            Init();
         }
 
-        public void Update()
+        private void Init()
         {
-            LightmapData[] lightmapData = LightmapSettings.lightmaps;
-            LightmapData[] combinedLightmaps = new LightmapData[m_Lightmaps!.Length + lightmapData.Length];
+            if (renderInfo_renderer == null || renderInfo_renderer.Length == 0 ||
+                renderInfo_lightmapIndex == null || renderInfo_lightmapOffsetScale == null ||
+                lightInfo_light == null || lightInfo_lightmapBaketype == null || lightInfo_mixedLightingMode == null ||
+                m_Lightmaps == null || m_LightmapsDir == null || m_ShadowMasks == null)
+                return;
 
-            Array.Copy(lightmapData, combinedLightmaps, lightmapData.Length);
-            for (int i = 0; i < m_Lightmaps.Length; i++)
+            // Create new LightmapData for all recorded lightmaps with their index
+            var newLightmaps = m_Lightmaps.Select((lm, i) => new LightmapData
             {
-                combinedLightmaps[lightmapData.Length + i] = new LightmapData
+                lightmapColor = m_Lightmaps[i],
+                lightmapDir = m_LightmapsDir[i],
+                shadowMask = m_ShadowMasks[i],
+            }).ToArray();
+
+            var combinedLightmaps = LightmapSettings.lightmaps.Concat(newLightmaps).ToArray();
+
+            bool directional = true;
+
+            foreach (Texture2D t in m_LightmapsDir!)
+            {
+                if (t == null)
                 {
-                    lightmapColor = m_Lightmaps[i]
-                };
+                    directional = false;
+                    break;
+                }
             }
 
-            ApplyRendererInfo(m_Renderers!, m_LightmapOffsetScales!, lightmapData.Length);
+            LightmapSettings.lightmapsMode = (m_LightmapsDir.Length == m_Lightmaps.Length && directional) ? LightmapsMode.CombinedDirectional : LightmapsMode.NonDirectional;
+
+            var rendererInfo = renderInfo_renderer.Select((r, i) => new RendererInfo // Use the struct in case future versions will succeeed
+            {
+                renderer = r,
+                lightmapIndex = renderInfo_lightmapIndex[i],
+                lightmapOffsetScale = renderInfo_lightmapOffsetScale[i],
+            }).ToArray();
+            ApplyRendererInfo(rendererInfo);
+
+            var lightInfo = lightInfo_light.Select((l, i) => new LightInfo // Use the struct in case future versions will succeeed
+            {
+                light = l,
+                lightmapBaketype = lightInfo_lightmapBaketype[i],
+                mixedLightingMode = lightInfo_mixedLightingMode[i],
+            }).ToArray();
+            ApplyLightInfo(lightInfo);
+
             LightmapSettings.lightmaps = combinedLightmaps;
         }
 
-        private static void ApplyRendererInfo(IReadOnlyList<Renderer> renderers, IReadOnlyList<Vector4> lightmapOffsetScales, int lightmapIndexOffset)
+        private void ApplyRendererInfo(RendererInfo[] rendererInfos)
         {
-            for (int i = 0; i < renderers.Count; i++)
+            foreach (var rendererInfo in rendererInfos)
             {
-                Renderer renderer = renderers[i];
-                renderer.lightmapIndex = i + lightmapIndexOffset;
-                renderer.lightmapScaleOffset = lightmapOffsetScales[i];
+                rendererInfo.renderer.lightmapIndex = rendererInfo.lightmapIndex;
+                rendererInfo.renderer.lightmapScaleOffset = rendererInfo.lightmapOffsetScale;
+
+                // Find common shaders
+                foreach (var sharedMaterial in rendererInfo.renderer.sharedMaterials.Where(sm => sm != null))
+                {
+                    var commonShader = Shader.Find(sharedMaterial.shader!.name);
+                    if (commonShader != null)
+                    {
+                        sharedMaterial.shader = commonShader;
+                    }
+                }
+
+            }
+        }
+
+        private void ApplyLightInfo(LightInfo[] lightInfos)
+        { 
+            foreach (var lightInfo in lightInfos)
+            {
+                LightBakingOutput bakingOutput = new LightBakingOutput();
+                bakingOutput.isBaked = true;
+                bakingOutput.lightmapBakeType = (LightmapBakeType)lightInfo.lightmapBaketype;
+                bakingOutput.mixedLightingMode = (MixedLightingMode)lightInfo.mixedLightingMode;
+
+                lightInfo.light.bakingOutput = bakingOutput;
             }
         }
     }
--- Unity/Script/CustomFloorPlugin/PrefabLightmapData.cs
+++ Unity/Script/CustomFloorPlugin/PrefabLightmapData.cs
@@ -6,9 +6,17 @@ namespace CustomFloorPlugin
     public class PrefabLightmapData : MonoBehaviour
     {
         // ReSharper disable InconsistentNaming
-        public Renderer[]? m_Renderers;
-        public Vector4[]? m_LightmapOffsetScales;
+        public Renderer[]? renderInfo_renderer;
+        public int[]? renderInfo_lightmapIndex;
+        public Vector4[]? renderInfo_lightmapOffsetScale;
+
         public Texture2D[]? m_Lightmaps;
+        public Texture2D[]? m_LightmapsDir;
+        public Texture2D[]? m_ShadowMasks;
+
+        public Light[]? lightInfo_light;
+        public int[]? lightInfo_lightmapBaketype;
+        public int[]? lightInfo_mixedLightingMode;
         // ReSharper restore InconsistentNaming
     }
 }
